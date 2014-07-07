8c7b65bfbfc1e04280546e953db4c38a67862882
tvaditya <chikki@adityas-MacBook-Pro.local>
2014-07-07 03:28:27 -0300

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,20 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by aditya on 6/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+- (void)evolveStep;
+- (void)countNeighbors;
+- (void)updateCreatures;
+
+@end
